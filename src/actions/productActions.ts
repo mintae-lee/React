519d94fcf05f57ee0b7a6945853771dcc10e6947
Mintae Lee <k-konno@sensyn-robotics.com>
2023-03-12 11:23:47 +0900

    replace project with new

---
+++ src/actions/productActions.ts
@@ -0,0 +1,52 @@
+/**
+ * FETCH_PRODUCTS_REQUEST, 
+ * FETCH_PRODUCTS_SUCCESS, 
+ * FETCH_PRODUCTS_FAILURE
+ * 라는 액션 타입들과 각각의 액션 생성 함수들을 정의합니다. 
+ * 이 파일에서는 TypeScript의 타입 시스템을 사용하여, 
+ * 액션 객체들이 갖는 속성과 값을 엄격하게 정의하고 있습니다.
+ */
+import { Product } from '../services/productService';
+
+export const FETCH_PRODUCTS_REQUEST = 'FETCH_PRODUCTS_REQUEST';
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+
+interface FetchProductsRequestAction {
+  type: typeof FETCH_PRODUCTS_REQUEST;
+}
+
+interface FetchProductsSuccessAction {
+  type: typeof FETCH_PRODUCTS_SUCCESS;
+  payload: Product[];
+}
+
+interface FetchProductsFailureAction {
+  type: typeof FETCH_PRODUCTS_FAILURE;
+  payload: string;
+}
+
+export type ProductActionTypes =
+  | FetchProductsRequestAction
+  | FetchProductsSuccessAction
+  | FetchProductsFailureAction;
+
+export const fetchProductsRequest = (): ProductActionTypes => {
+  return {
+    type: FETCH_PRODUCTS_REQUEST,
+  };
+};
+
+export const fetchProductsSuccess = (products: Product[]): ProductActionTypes => {
+  return {
+    type: FETCH_PRODUCTS_SUCCESS,
+    payload: products,
+  };
+};
+
+export const fetchProductsFailure = (error: string): ProductActionTypes => {
+  return {
+    type: FETCH_PRODUCTS_FAILURE,
+    payload: error,
+  };
+};
---
+++ src/containers/ProductContainer.tsx
@@ -0,0 +1,36 @@
+/**
+ * ProductList 컴포넌트를 렌더링할 때 products 상태를 props로 전달합니다. 
+ * 이렇게 함으로써 ProductList 컴포넌트는 ProductItem 컴포넌트를 
+ * 반복적으로 렌더링하면서 products 데이터를 출력하게 됩니다.
+ */
+import React, { useEffect, useState } from 'react';
+import ProductList from '../components/ProductList';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+// ProductContainer 컴포넌트 정의
+const ProductContainer: React.FC = () => {
+  // useState를 사용하여 products 상태를 관리
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    // useEffect를 사용하여 컴포넌트가 마운트되었을 때 
+    // API로부터 데이터를 불러와 products 상태를 업데이트
+    fetch('https://myapi.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>My Products</h1>
+      <ProductList products={products} />
+    </div>
+  );
+};
+
+export default ProductContainer;
